f960502343a9d93493628c0d441a01e756359ba4
Christoph Retter <christoph.retter@icloud.com>
2019-05-28 12:56:37 +0200

    added index html

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 const http = require('http');
+var fs = require('fs');
 const port=process.env.PORT || 3000
 const server = http.createServer((req, res) => {
 res.statusCode = 200;
 res.setHeader('Content-Type', 'text/html');
-res.end('<h1>Hello World</h1>');
+fs.readFile('./index.html', null, function(error, data) {
+    if (error) {
+        response.writeHead(404);
+        response.write('File not found!');
+    } else {
+        response.write(data);
+    }
+    res.end();
+});
 });
 server.listen(port,() => {
 console.log(`Server running at port `+port);
