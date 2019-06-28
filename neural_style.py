866140377989f5f3359a6d7224874dd89d89d319
Christoph Retter <christoph.retter@icloud.com>
2019-06-28 10:02:57 +0200

    Made Code leaner

--- neural_style.py
+++ neural_style.py
@@ -1,15 +1,7 @@
-import argparse
 import os
-import sys
-import time
-import datetime
 import re
 
-import numpy as np
 import torch
-from torch.optim import Adam
-from torch.utils.data import DataLoader
-from torchvision import datasets
 from torchvision import transforms
 import torch.onnx
 
